ffdeda053ce5d666c472b35e6c214079fce6e9b5
Eyad Ahmed <Eyad932004@outlook.com>
2025-01-03 18:37:42 +0200

    feat(hero): Add Hero component with introductory content and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Header from "./components/sections/Header";
+import Hero from "./components/sections/Hero";
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   );
 };
