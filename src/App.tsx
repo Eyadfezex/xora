251759fb1a44762c5f0ba63ae79138604bc3b582
Eyad Ahmed <Eyad932004@outlook.com>
2025-01-07 17:54:48 +0200

    feat(pricing): Add Pricing component with flexible pricing plans and toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Features from "./components/sections/Features";
 import Header from "./components/sections/Header";
 import Hero from "./components/sections/Hero";
+import Pricing from "./components/sections/Pricing";
+
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   );
 };
